59a909f6c9394b10e440a5bd5171e81fb000dbf5
Your Name <you@example.com>
2021-10-28 07:50:01 +0530

    added image with text description

--- MyApplication/app/src/main/java/com/example/myapplication/adapter/ItemAdapter.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/adapter/ItemAdapter.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.R
@@ -23,7 +24,8 @@ class ItemAdapter(
     // you provide access to all the views for a data item in a view holder.
     // Each data item is just an Affirmation object.
     class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
-        val textView: TextView = view.findViewById(R.id.item_title)
+        val textView: TextView = view.findViewById(R.id.item_title);
+        val imageView: ImageView = view.findViewById(R.id.item_image);
     }
 
     /**
@@ -42,7 +44,8 @@ class ItemAdapter(
      */
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = dataset[position]
-        holder.textView.text = context.resources.getString(item.stringResourceId)
+        holder.textView.text = context.resources.getString(item.stringResourceId);
+        holder.imageView.setImageResource(item.imageResourceId);
     }
 
     /**
