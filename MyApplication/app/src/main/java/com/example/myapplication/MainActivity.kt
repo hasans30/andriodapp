43fdc555b6c380295532a6d64b8bbbd7cc684b24
Your Name <you@example.com>
2021-10-28 07:26:31 +0530

    first commit

---
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.adapter.ItemAdapter
+import com.example.myapplication.data.DataSource
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main);
+        val myDataset = DataSource().loadAffirmations();
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view);
+        recyclerView.adapter = ItemAdapter(this, myDataset);
+        recyclerView.setHasFixedSize(true);
+        
+       }
+}
